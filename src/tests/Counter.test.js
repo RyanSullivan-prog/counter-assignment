d47444470e0a65de3e6773cc7863fc3c2c117e7f
RyanSullivan-prog <82338284+RyanSullivan-prog@users.noreply.github.com>
2023-04-04 11:14:28 -0400

    Add tests with 100% passing coverage

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,41 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
+import { act } from 'react-dom/test-utils';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  //render(<Counter />);
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
+  const myCount = screen.getByTestId(/count/i);
+  expect(myCount.innerHTML).toBe("0");
   // Complete the unit test below based on the objective in the line above
 });
 
 test('clicking + increments the count', () => {
+  const myButton = screen.getByText(/\+/i);
+  const myCount = screen.getByTestId(/count/i);
+  var intTemp = parseInt(myCount.innerHTML);
+  act(() => {
+    myButton.click();
+  });
+  expect(parseInt(myCount.innerHTML)).toBe(intTemp+1);
   // Complete the unit test below based on the objective in the line above
 });
 
 test('clicking - decrements the count', () => {
+  const myButton = screen.getByText(/\-/i);
+  const myCount = screen.getByTestId(/count/i);
+  var intTemp = parseInt(myCount.innerHTML);
+  act(() => {
+    myButton.click();
+  });
+  expect(parseInt(myCount.innerHTML)).toBe(intTemp-1);
   // Complete the unit test below based on the objective in the line above
 });
